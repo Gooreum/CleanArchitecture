e0231c96c141d3e2df1ba6c25f9cb15873aea056
Mingu Seo <tjalsrnek@gmail.com>
2021-09-24 18:50:56 +0900

    [add] set folders

---
+++ CleanArchitecture/CleanArchitecture/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  CleanArchitecture
+//
+//  Created by Mingu Seo on 2021/09/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    
+
+}
+
