d0a7c5a12b6b983374a129b01dbbd34a84c110d7
Mingu Seo <tjalsrnek@gmail.com>
2021-09-26 05:40:06 +0900

    [update] movie model

--- CleanArchitecture/CleanArchitecture/Model/Movies.swift
+++ CleanArchitecture/CleanArchitecture/Model/Movies.swift
@@ -5,26 +5,15 @@
 //  Created by Mingu Seo on 2021/09/24.
 //
 
-
-struct Movies {
-    enum CodingKeys: String, CodingKey {
-        case id
-        case title
-        case releaseDate = "release_date"
-        case overview
-    }
+struct Movie: Codable {
     let id: Int
-    let title: String
-    let releaseDate: String
+    let title : String
     let overview: String
+    let release_date: String
 }
 
-extension Movies: Decodable {
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        id = try container.decode(Int.self, forKey: .id)
-        title = try container.decode(String.self, forKey: .title)
-        releaseDate = try container.decode(String.self, forKey: .releaseDate)
-        overview = try container.decode(String.self, forKey: .overview)
-    }
+struct MovieResponse:Codable {
+    let page: Int
+    let dates: [String: String]
+    let results: [Movie]
 }
