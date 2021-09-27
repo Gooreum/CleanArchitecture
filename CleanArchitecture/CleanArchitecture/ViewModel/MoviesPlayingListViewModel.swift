b9974c3552c8dcd5117474c04e26602f846878ad
Mingu Seo <tjalsrnek@gmail.com>
2021-09-27 15:22:36 +0900

    [update] MoviesPlayingListViewModel

--- CleanArchitecture/CleanArchitecture/ViewModel/MoviesPlayingListViewModel.swift
+++ CleanArchitecture/CleanArchitecture/ViewModel/MoviesPlayingListViewModel.swift
@@ -7,16 +7,48 @@
 
 import Foundation
 import RxSwift
+import Action
 
-enum error: String,Error {
-    case noValue = "값 없음"
-}
-
-public class MoviesPlayingListViewModel {
+class MoviesPlayingListViewModel {
     
-    private let webService = WebService()
     
-    var moviesPlayingList: Observable<[Movie]> {
-        return webService.fetchMoviesPlaying()
+    private let webService: WebService
+    
+    init() {
+        self.webService = WebService()
     }
+    
+    func convertMoviesPlayingDataAsStream(page: Int)-> Observable<[Movie]> {
+        return Observable.create { observer in
+
+            self.webService.fetchMoviesPlaying(page: page) { (movies, error) in
+                if let error = error {
+                    print("Failed request from themoviedb: \(error.localizedDescription)")
+                    observer.onError(WebError.failedRequest)
+                }
+                if let movies = movies {
+                    print(movies)
+                    observer.onNext(movies)
+                }
+                observer.onCompleted()
+            }
+            return Disposables.create()
+        }
+    }
+//    var moviePlayingList: Observable<[Movie]> {
+//        return storage.playingMovieList(page: 1)
+//    }
+    
+    //클로저 내부에서 셀프로 접근해야 되기 때문에 lazy로 선언
+//    lazy var detailAction: Action<Movie, Void> = {
+//        return Action { movie in
+//            //디테일 뷰 모델 생성
+//            let movieViewModel = MovieViewModel(movie: movie, sceneCoordinator: self.sceneCoordinator, storage: self.storage)
+//
+//            //Scene 생성
+//            let movieDetailScene = Scene.movieDetail(movieViewModel)
+//
+//            return self.sceneCoordinator.transition(to: movieDetailScene, using: .push, animated: true).asObservable().map { _ in }
+//        }
+//    }()    
 }
