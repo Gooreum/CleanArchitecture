1fa89ece8050deede3ef36271476b93c29429977
Mingu Seo <tjalsrnek@gmail.com>
2021-09-29 14:31:12 +0900

    [update] MovieDetailViewController

--- CleanArchitecture/CleanArchitecture/View/MovieDetailViewController.swift
+++ CleanArchitecture/CleanArchitecture/View/MovieDetailViewController.swift
@@ -9,33 +9,55 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import CoreData
 
 class MovieDetailViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var btnSave: UIBarButtonItem!
     
     private let disposeBag = DisposeBag()
     private var viewModel = MovieViewModel()
-    var id: Int?
-        
+    var movie: MyMovie?
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpBinding()
     }
     
     private func setUpBinding() {
-        if let id = self.id {
+        if let id = self.movie?.id {
             viewModel.convertMovieDataAsStream(id: id)
                 .bind(to: tableView.rx.items(cellIdentifier: "MovieDetailCell", cellType: MovieDetailTableViewCell.self)) { [weak self] (_, movie, cell) in
                     self?.setupCell(cell, movie: movie)
                 }
                 .disposed(by: disposeBag)
-        }else {            
+        }else {
             print("No ID")
+        }
+        
+        if let movie = self.movie {
+            viewModel.checkMovieInStorage(movie: movie)
+                .subscribe(onNext: {
+                    switch $0 {
+                    case true :
+                        print("TRUE")
+                        self.btnSave.rx.action = self.viewModel.performSave(movie: movie)
+                        self.btnSave.title = "저장"
+                    case false :
+                        print("FALSE")
+                        self.btnSave.rx.action = self.viewModel.performDelete(movie: movie)
+                        self.btnSave.title = "삭제"
+                    }
+                })
+                .disposed(by: disposeBag)
+            
+            
         }
     }
     
-    private func setupCell(_ cell: MovieDetailTableViewCell, movie: Movie) {
+    private func setupCell(_ cell: MovieDetailTableViewCell, movie: MyMovie) {
         cell.selectionStyle = .none
         cell.setTitle(movie.title ?? "")
         cell.setOverview(movie.overview ?? "")
