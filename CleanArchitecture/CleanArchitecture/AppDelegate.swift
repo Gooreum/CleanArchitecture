1adba894db672d30828aec53936dd15971f140f9
Mingu Seo <tjalsrnek@gmail.com>
2021-10-05 17:15:43 +0900

    [추가] Swinject를 이용하여 AppDelegate에서 ViewController마다 필요한 ViewModel의 의존성 주입 및, ViewModel의 WebServiceType/MovieStorageType에 대한 의존성 주입 완료.

--- CleanArchitecture/CleanArchitecture/AppDelegate.swift
+++ CleanArchitecture/CleanArchitecture/AppDelegate.swift
@@ -6,18 +6,59 @@
 //
 
 import UIKit
-
+import Swinject
+import SwinjectStoryboard
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    var container: Container = {
+        let container = Container()
+        
+        container.register(WebServiceType.self) { _ in WebServiceImpl() }
+        container.register(MovieStorageType.self) { _ in MovieStorageImpl(modelName: "Model") }
+        
+        //ViewModel
+        container.register(CommonViewModel.self, name: "MoviesPlayingListViewModel") { r in
+            let viewModel = MoviesPlayingListViewModel()            
+            viewModel.webService = r.resolve(WebServiceType.self)
+            viewModel.storage = r.resolve(MovieStorageType.self)
+            return viewModel
+        }
+        container.register(CommonViewModel.self, name: "MovieViewModel") { r in
+            let viewModel = MovieViewModel()
+            viewModel.webService = r.resolve(WebServiceType.self)
+            viewModel.storage = r.resolve(MovieStorageType.self)
+            return viewModel
+        }
+        
+        container.register(CommonViewModel.self, name: "MyMovieListViewModel") { r in
+            let viewModel = MyMovieListViewModel()
+            viewModel.webService = r.resolve(WebServiceType.self)
+            viewModel.storage = r.resolve(MovieStorageType.self)
+            return viewModel
+        }
+                
+        container.storyboardInitCompleted(MoviesPlayingListViewController.self) { r, c in
+            c.viewModel = r.resolve(CommonViewModel.self, name: "MoviesPlayingListViewModel") as! MoviesPlayingListViewModel
+        }
+        
+        container.storyboardInitCompleted(MovieDetailViewController.self) { r, c in
+            c.viewModel = r.resolve(CommonViewModel.self, name: "MovieViewModel") as? MovieViewModel
+        }
+        
+        container.storyboardInitCompleted(MyMovieListViewController.self) { r, c in
+            c.viewModel = r.resolve(CommonViewModel.self, name: "MyMovieListViewModel") as? MyMovieListViewModel
+        }
+        return container
+    }()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+     
+        let storyboard = SwinjectStoryboard.create(name: "Main", bundle: nil, container: container)
+        self.window?.rootViewController = storyboard.instantiateInitialViewController()
+        
         return true
     }
-
-    
-
 }
 
