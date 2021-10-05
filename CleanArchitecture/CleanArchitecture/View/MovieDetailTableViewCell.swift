696f18fb57ac2f09f336bb250a3ea21ce6f301dc
Mingu Seo <tjalsrnek@gmail.com>
2021-10-05 17:14:02 +0900

    [수정] 영화상세정보 보여주는 일부 라벨 삭제

--- CleanArchitecture/CleanArchitecture/View/MovieDetailTableViewCell.swift
+++ CleanArchitecture/CleanArchitecture/View/MovieDetailTableViewCell.swift
@@ -12,10 +12,6 @@ class MovieDetailTableViewCell: UITableViewCell {
     @IBOutlet weak var imageMovie: UIImageView!
     @IBOutlet weak var labelTitle: UILabel!
     @IBOutlet weak var labelReleaseDate: UILabel!
-    @IBOutlet weak var labelrevenue: UILabel!
-    @IBOutlet weak var labelGenres: UILabel!
-    @IBOutlet weak var labelVoteAverage: UILabel!
-    @IBOutlet weak var labelRuntime: UILabel!
     @IBOutlet weak var labelOverview: UILabel!
     
     func setTitle(_ title: String) {
@@ -24,23 +20,7 @@ class MovieDetailTableViewCell: UITableViewCell {
     
     func setReleaseDate(_ releaseDate: String) {
         labelReleaseDate.text = releaseDate
-    }
-    
-    func setRevenue(_ revenue: String) {
-        labelrevenue.text = revenue
-    }
-    
-    func setGeneres(_ genre: String) {
-        labelGenres.text = genre
-    }
-    
-    func setVoteAverage(_ voteAverage: String) {
-        labelVoteAverage.text = voteAverage
-    }
-    
-    func setRuntime(_ runtime: String) {
-        labelRuntime.text = runtime
-    }
+    }    
     
     func setOverview(_ overview: String) {
         labelOverview.text = overview
