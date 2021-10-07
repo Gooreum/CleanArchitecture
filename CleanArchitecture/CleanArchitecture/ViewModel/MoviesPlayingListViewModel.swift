ad9d10a2cdc71f0f27b1d8c2093cd898a0d063d7
Mingu Seo <tjalsrnek@gmail.com>
2021-10-08 03:06:53 +0900

    [추가] 페이징 처리 - 페이징 처리하여 새로 가져온 영화 데이터를 moviesSubject(BehaviorSubject)가 기존에 가지고 있던 영화 데이터를 합쳐서 다시 moviesSubject에게 onNext로 보내준다. - 그런데 이렇게 해도 되는 것인지 모르겠다. 	- 페이지 1에서 영화A를 받아 moviesSubject에 onNext시켜 테이블뷰에 바인딩 시키고, 	- 페이지 2에서 영화B를 받아 영화A+영화B를  moviesSubject에 onNext 한뒤 테이블뷰에 바인딩하면, 	- 테이블뷰에서 영화A, 영화B 뷰를 새롭게 다시 그리게 되는게 아닌가 싶음.

--- CleanArchitecture/CleanArchitecture/ViewModel/MoviesPlayingListViewModel.swift
+++ CleanArchitecture/CleanArchitecture/ViewModel/MoviesPlayingListViewModel.swift
@@ -10,44 +10,80 @@ import RxSwift
 import Action
 import RxCocoa
 
-class MoviesPlayingListViewModel: CommonViewModel {
+class MoviesPlayingListViewModel {
     
     let disposeBag = DisposeBag()
-        
-    let activated: Observable<Bool>
+    
     let moviesSubject = BehaviorSubject<[Movie]>(value: [])
     
-    let fetching = PublishSubject<Void>()
-    private let activating = BehaviorSubject<Bool>(value: false)
+    //refresh
+    private let refresshingActivating = BehaviorSubject<Bool>(value: false)
+    let refreshingActivated: Observable<Bool>
+    let refreshingfetching = PublishSubject<Void>()
     
-    //MARK: TODO - 네트워크 상태에 따른 데이터 처리
-    //1.네트워크 상태를 체크하는 Util을 새로 만든다.(의존성 주입을 받는다.)
-    //2.상태에 따라 API 통신할지, 로컬DB에서 가져온다.
-        
-    var webService: WebServiceType?
-    var storage: MovieStorageType?
-    var networkStateUtil: NetworkState?
+    //pagination
+    private let paginationActivating = BehaviorSubject<Bool>(value: false)
+    private var page: Int = 1
+    let paginationActivated: Observable<Bool>
+    let paginationfetching = PublishSubject<Void>()
+    var pageSubject = BehaviorSubject<Int>(value: 1)
+    
+    var webService: WebServiceType
+    var storage: MovieStorageType
+    var networkStateUtil: NetworkState
+    
     
     init(webService: WebServiceType, storage: MovieStorageType, networkStateUtil: NetworkState) {
         self.webService = webService
         self.storage = storage
         self.networkStateUtil = networkStateUtil
         
-        //let state = self.networkStateUtil?.monitorReachability()
-//        print(state)
+        refreshingActivated = refresshingActivating.distinctUntilChanged()
+        paginationActivated = paginationActivating.distinctUntilChanged()
         
-        activated = activating.distinctUntilChanged()
-        fetching
-            .do(onNext: { self.activating.onNext(true)})
-            //.flatMap {self.webService!.fetchMoviesPlayingRx(page: 1)}
+        //새로고침 처리
+        refreshingfetching
+            .do(onNext: { self.refresshingActivating.onNext(true)})
             .flatMapLatest {
                 Observable.deferred {
-                    self.networkStateUtil?.monitorReachability() == true ? self.webService!.fetchMoviesPlayingRx(page: 1) : self.storage!.myMovieList()
+                    networkStateUtil.monitorReachability() == true ? webService.fetchMoviesPlayingRx(page: 1) : storage.myMovieList()
                 }
             }
-            .do(onNext: { _ in
-                self.activating.onNext(false) })
-            .subscribe(onNext: {self.moviesSubject.onNext($0)})
+            .do(onNext: { _ in self.refresshingActivating.onNext(false) })
+            .subscribe(onNext: { [weak self] in
+                self?.moviesSubject.onNext($0)
+                //페이지 추가
+                self?.page += 1
+                
+            })
             .disposed(by: disposeBag)
-    }    
+        
+        //페이징 처리
+        paginationfetching
+            .do(onNext: { self.paginationActivating.onNext(true)})
+                .flatMap {
+                    Observable.deferred { webService.fetchMoviesPlayingRx(page: self.page) }
+                }
+                .debug()
+                .do(onNext: { _ in self.paginationActivating.onNext(false) })
+                    .subscribe(onNext: {[weak self] in
+                        do {
+                            //moviesSubject(BehaviorSubject)가 가지고 있던 기존 영화 데이터들을 새로운 영화 데이터와 합쳐준다..
+                            //이렇게 해도 되는건가..
+                            let oldMovies = try self?.moviesSubject.value() ?? [Movie]()
+                            self?.moviesSubject.onNext(oldMovies + $0)
+                        }catch(let error) {
+                            print(error)
+                            self?.moviesSubject.onError(error)                            
+                        }
+                        //페이지 추가
+                        self?.page += 1
+                    })
+                    .disposed(by: disposeBag)
+                    }
 }
+
+/*
+ 페이징 처리해야 될 작업
+ 1.액티비티 인디케이터 보여주기 / 숨기기
+ */
