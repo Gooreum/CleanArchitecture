b9fbb20e2221f234c141bec5a925fe6bba1fb44c
Mingu Seo <tjalsrnek@gmail.com>
2021-10-20 23:11:06 +0900

    [추가] movieDetailEntity 추가

--- CleanArchitecture/CleanArchitecture/DomainLayer/Entities/Movie.swift
+++ CleanArchitecture/CleanArchitecture/DomainLayer/Entities/Movie.swift
@@ -6,72 +6,11 @@
 //
 
 import Foundation
-import CoreData
-import RxCoreData
-import RxDataSources
 
-struct MoviePlayingResponse: Decodable {
-    let page: Int
-    let dates: [String: String]
-    let results: [Movie]
-}
-
-struct MovieResponse: Decodable {
-    let movie: [Movie]
-}
-
-//MARK: CoreData object model
-struct Movie: Decodable {
+struct MovieDetailEntity : Entity{
     let id: Int
-    let overview: String
-    let release_date: String
-    let title: String
-    var poster_path: String? = ""
-}
-
-extension Movie : Equatable {
-    static func == (lhs: Movie, rhs: Movie) -> Bool {
-        return lhs.id == rhs.id
-    }
-}
-
-extension Movie : IdentifiableType {
-    typealias Identity = String
-   
-    var identity: Identity { return "\(id)" }
-}
-
-extension Movie : Persistable {
-    
-    typealias T = NSManagedObject
-    
-    static var entityName: String {
-        return "Movie"
-    }
-    
-    static var primaryAttributeName: String {
-        return "id"
-    }
-    
-    init(entity: T) {
-        id = entity.value(forKey: "id") as! Int
-        release_date = entity.value(forKey: "release_date") as! String
-        overview = entity.value(forKey: "overview") as! String
-        title = entity.value(forKey: "title") as! String
-        poster_path = entity.value(forKey: "poster_path") as? String
-    }
-    
-    func update(_ entity: T) {
-        entity.setValue(id, forKey: "id")
-        entity.setValue(release_date, forKey: "release_date")
-        entity.setValue(overview, forKey: "overview")
-        entity.setValue(title, forKey: "title")
-        entity.setValue(poster_path, forKey: "poster_path")
-        
-        do {
-            try entity.managedObjectContext?.save()
-        } catch let e {
-            print(e)
-        }
-    }
+    let title: String?
+    let posterPath: String?
+    let overview: String?
+    let releaseDate: String?
 }
