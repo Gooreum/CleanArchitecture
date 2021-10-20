f77cb7e702946db46519377882dfcaa568623770
Mingu Seo <tjalsrnek@gmail.com>
2021-10-20 23:07:33 +0900

    [수정] 패키지 구성

---
+++ CleanArchitecture/CleanArchitecture/DomainLayer/UseCases/DataProvider/MoviesPlaying/MoviesPlayingUseCase.swift
@@ -0,0 +1,14 @@
+//
+//  FetchMoviesPlayingUseCase.swift
+//  CleanArchitecture
+//
+//  Created by Mingu Seo on 2021/10/18.
+//
+
+import Foundation
+import RxSwift
+
+protocol MoviesPlayingUseCase {
+    
+        
+}
---
+++ CleanArchitecture/CleanArchitecture/DomainLayer/UseCases/User/MyMovies/MyMoviesUseCaseImpl.swift
@@ -0,0 +1,8 @@
+//
+//  FetchMyMoviesUseCaseImpl.swift
+//  CleanArchitecture
+//
+//  Created by Mingu Seo on 2021/10/18.
+//
+
+import Foundation
